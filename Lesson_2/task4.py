1e491b1f193250ea8db02045028d229409cf0a6d
SprynchanYelysaveta <lizok.sprynchan@gmail.com>
2022-10-25 18:14:25 +0300

    Added lesson 2 and task 4

---
+++ Lesson_2/task4.py
@@ -0,0 +1,6 @@
+Number = int(input("Enter your number: "))
+Factorial = 1 
+while Number > 1: 
+    Factorial = Factorial * Number
+    Number = Number - 1
+print("The factorial of your number is: ", Factorial)
